ffa5dd24fc7710c6fec38640c7d6db29eb0d90bd
floatin___heart <prajaktapagar7@gmail.com>
2023-12-17 10:17:25 +0530

    state of multiple vars controlled as object

--- event-practice/src/App.js
+++ event-practice/src/App.js
@@ -1,21 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 const App = () => {
-  const [bg, setBg] = useState('purple');
-  const [name, setName] = useState('Hi Praju');
-  const bgChange = () =>{
-    setBg("pink");
-    setName("Welcome to React");
-  }
-  const bgBack = () =>{
-    setBg("purple");
-    setName("Hi Praju");
-  }
-  return(
+  const [bg, setBg] = useState({
+    color: "purple",
+    msg: "Hi Praju",
+  });
+  const bgChange = () => {
+    setBg(() => {
+      return {
+        color: "pink",
+        msg: "Welcome to React",
+      };
+    });
+  };
+  const bgBack = () => {
+    setBg(() => {
+      return {
+        color: "purple",
+        msg: "Hi Praju",
+      };
+    });
+  };
+  return (
     <>
-     <div style={{backgroundColor : bg, textAlign : "center"}}>
-      <button style={{fontSize : "larger"}} onClick={bgChange} onDoubleClick={bgBack}>{name}</button>
-     </div>
+      <div style={{ backgroundColor: bg.color, textAlign: "center" }}>
+        <button
+          style={{ fontSize: "larger" }}
+          onClick={bgChange}
+          onDoubleClick={bgBack}
+        >
+          {bg.msg}
+        </button>
+      </div>
     </>
   );
-}
+};
 export default App;
